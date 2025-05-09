17b3781df3a1a0d11772b90ef110426f020f71db
Bharti kumari gupta <shadhviraj97099@gmail.com>
2025-05-09 20:27:28 +0530

    initial commit

---
+++ cmd/projectname/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Bharti let's start the journey")
+}
+
+//
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bhartikumarigupta/Student1-api //module name
+
+go 1.23.0 //go version
